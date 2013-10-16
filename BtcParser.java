ba55d777a6c7e295c42b376a35af68f56417830b
reecek <reecekol@gmail.com>
2013-10-16 16:33:16 -0400

    json object

--- BtcParser.java
+++ BtcParser.java
@@ -1,4 +1,23 @@
+import javax.servlet.http.HttpUtils;
 
+import org.json.JSONObject;
+
+/*
+ *  Kole Reece & Andre Haynes 
+ * 
+ *  10/16/2013
+ */
 public class BtcParser {
 
+	public String apiKey="";
+	
+	private String result="";
+	
+	private static final String URL="";
+	
+	public JSONObject doPost()
+	{
+		result=HttpUtil.httpPost(URL);
+		
+	}
 }
